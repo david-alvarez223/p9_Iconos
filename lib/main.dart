47597adc93069f523f5bd6f9a23c6755d60f408a
David_Alvarez <a.21308051280310@cbtis128.edu.mx>
2025-02-26 20:11:32 +0000

    main

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(MisIconosApp());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+class MisIconosApp extends StatelessWidget {
+  const MisIconosApp([Key? key]) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
-        ),
-      ),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text("Alvarez David"),
+            titleTextStyle: TextStyle(
+                color: Colors.white54,
+                fontSize: 30,
+                fontStyle: FontStyle.italic),
+            centerTitle: true,
+            backgroundColor: Colors.black87,
+          ),
+          body: Column(children: <Widget>[
+            Text('Alvarez Lujan'),
+            Text('Mat: 21308051280310'),
+            SizedBox(height: 200),
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                Icon(
+                  Icons.coffee,
+                  size: 40.0,
+                ),
+                Icon(
+                  Icons.cookie,
+                  size: 40.0,
+                ),
+                Icon(
+                  Icons.cake,
+                  size: 40.0,
+                )
+              ],
+            )
+          ])),
     );
   }
 }
